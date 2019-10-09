a7d5e9b63efa7ef53f935f01e414edad7b72455a
rompatel <38078139+rompatel@users.noreply.github.com>
2019-10-09 19:17:48 +0530

    Create fun.cpp

---
+++ fun.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);srand(time(NULL));
+#define ll long long int
+#define ull unsigned long long int
+#define db long double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define deb1(n) cout<<n<<"\n"
+#define deb2(a,b) cout<<a<<" "<<b<<"\n"
+#define deba(a,n) for(i=0;i<n;++i) {  cout<<a[i]<<" "; };cout<<"\n"
+#define deb3(a,b,c) cout<<a<<" "<<b<<" "<<c<<"\n"
+#define MOD 1000000007
+#define inf LLONG_MAX
+#define PI 3.141592653589793238463
